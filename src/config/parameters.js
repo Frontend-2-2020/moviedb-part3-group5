dac3921253adaa349a67a0a31842e0950cfd57de
Tim Vanloocke <tim.vanloocke@roeselare.be>
2020-01-06 20:40:37 +0100

    3de commit Tim

---
+++ src/config/parameters.js
@@ -0,0 +1,10 @@
+export const API_key = "dfd139ed073a8beb86b06c8abd904715";
+export const URL_overview =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
+  API_key +
+  "&page=";
+export const URL_detail = "https://api.themoviedb.org/3/movie/";
+export const URL_image = "https://image.tmdb.org/t/p/";
+export const URL_website = "http://localhost:8080/index.html";
+export const URL_genre =
+  "https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_key;
