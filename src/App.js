0d735f0ea0f80180dd6e54fa3fa0c86d1f3f75c7
bjille <tim.vanloocke@gmail.com>
2020-01-09 17:06:38 +0100

    tryout jumbotron detail pagina

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react'
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link
-  } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-  import Home from './pages/Home';
-  import Detail from './pages/Detail';
-  import Page404 from './pages/Page404';
-  import 'bootstrap/dist/css/bootstrap.css';
- 
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Page404 from "./pages/Page404";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-          <Switch>
-              <Route path="/movie/:id" component={Detail}></Route>
-              <Route path="/" exact component={Home}></Route>
-			  <Route path="/" component={Page404}></Route>
-          </Switch>
+        <Switch>
+          <Route path="/movie/:id" component={Detail}></Route>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/" component={Page404}></Route>
+        </Switch>
       </Router>
-    )
+    );
   }
 }
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { URL_detail, API_key } from "../config/parameters";
+import axios from "axios";
 
 export default class Detail extends Component {
-    render() {
-        return (
-            <div>
-                Detail
-            </div>
-        )
-    }
+  state = {};
+  componentDidMount() {
+    const movieId = this.props.match.params.id;
+    // const movieId = 1;
+    axios.get(URL_detail + movieId + "?api_key=" + API_key).then(res => {
+      this.setState({ movieDetail: res.data });
+      this.setState({
+        backdrop: "https://image.tmdb.org/t/p/w1280/" + res.data.backdrop_path
+      });
+    });
+  }
+
+  render() {
+    const jumbotronStyle = {
+      //   backgroundImage: "url(" + this.state.backdrop + ")"
+      //   backgroundImage:
+      //     "url(https://image.tmdb.org/t/p/w1280/" + this.state.moviedetail.backdrop_path + ")"
+      //   backgroundColor: "rgba(52, 52, 52, 0.5)",
+      backgroundImage: `url(${this.state.backdrop})`,
+      backgroundSize: "cover",
+      position: "relative"
+    };
+    return (
+      <div className="container">
+        <div style={jumbotronStyle} className="jumbotron">
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(255, 255, 255, 0.5)"
+            }}
+          />
+        </div>
+        {/* <img src={this.state.backdrop} alt="" /> */}
+      </div>
+    );
+  }
 }
